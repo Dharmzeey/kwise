00fa44c92e8fcf083147a8f2b3b0a62a5198ea37
Adam Olanrewaju <olanrewajuadam2020@gmail.com>
2024-11-20 19:13:22 +0100

    Implemented toast instead of alert(), removed refresh and useing access token alone, also implemented AuthContext

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,5 +5,4 @@ export const IS_AUTHENTICATED = true;
 export const ACCESS_TOKEN_MAX_AGE = 60 * 60 * 24 * 14; // 14 days
 export const REFRESH_TOKEN_MAX_AGE = 60 * 60 * 30; // 1 day
 export const SESSION_TOKEN_MAX_AGE = 60 * 60 * 24 * 14; // 14 days
-
 export const SESSION_ID = "sessionid";
